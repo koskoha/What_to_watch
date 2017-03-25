b510db5acde16de4860dff362c201f6f513fee5e
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2017-03-25 13:11:02 -0700

    link ferebase to project

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA_fu2UaPSlieTOLsBfHF0RnqUYrJVS1WE",
+    authDomain: "what-to-watch-db8c3.firebaseapp.com",
+    databaseURL: "https://what-to-watch-db8c3.firebaseio.com",
+    storageBucket: "what-to-watch-db8c3.appspot.com",
+    messagingSenderId: "286455706703"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+  /*  ------------------------Code goes here---------------------------------------------*/
+

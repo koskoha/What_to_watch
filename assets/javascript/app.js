c8ec3d623be061502979ccfdf3a6458cd703b206
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2017-03-29 13:16:57 -0700

    get 3 photos and web cam snap

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,38 @@
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyA_fu2UaPSlieTOLsBfHF0RnqUYrJVS1WE",
-    authDomain: "what-to-watch-db8c3.firebaseapp.com",
-    databaseURL: "https://what-to-watch-db8c3.firebaseio.com",
-    storageBucket: "what-to-watch-db8c3.appspot.com",
-    messagingSenderId: "286455706703"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA_fu2UaPSlieTOLsBfHF0RnqUYrJVS1WE",
+  authDomain: "what-to-watch-db8c3.firebaseapp.com",
+  databaseURL: "https://what-to-watch-db8c3.firebaseio.com",
+  storageBucket: "what-to-watch-db8c3.appspot.com",
+  messagingSenderId: "286455706703"
+};
+firebase.initializeApp(config);
 
 
+      
 
+/*  ------------------------Code goes here---------------------------------------------*/
 
-  /*  ------------------------Code goes here---------------------------------------------*/
+var threeImg = [];
 
+function readURL(input) {
+	if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = addPicToPage;
+    	reader.readAsDataURL(input.files[0]);
+  	}
+}
+
+function addPicToPage(img){
+	threeImg.push(img.target.result);
+	var imgBlock = $('<div class="block">')
+	var $image = $('<img>')
+  	$image.attr('src', img.target.result)
+  	.width(150)
+  	.height(200);
+  	$('#img-block').append(imgBlock.append($image));
+	if (threeImg.length === 3) {
+		$('#picture-uploader').prop("disabled", true);
+	}
+}

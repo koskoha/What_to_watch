d4f95539b79acabb6e5389cdeebc34921867fe12
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2017-04-06 16:48:49 -0400

    adding fixes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,11 +25,73 @@ var facialKeywordObject;
 //paragraph emonions keyword 
 var paragraphKeyword;
 var paragraphKeywordObject;
+var sortedParagraphKeywordArray;
 
 var indicoAPIkey = "7757e7fda1d6a89efbd1cc07880cc78f";
 
+//Array of movie objects
+var genreArray = [{ "id": 28, "name": "Action" }, { "id": 12, "name": "Adventure" }, { "id": 16, "name": "Animation" }, { "id": 35, "name": "Comedy" }, { "id": 80, "name": "Crime" }, { "id": 99, "name": "Documentary" }, { "id": 18, "name": "Drama" }, { "id": 10751, "name": "Family" }, { "id": 14, "name": "Fantasy" }, { "id": 36, "name": "History" }, { "id": 27, "name": "Horror" }, { "id": 10402, "name": "Music" }, { "id": 9648, "name": "Mystery" }, { "id": 10749, "name": "Romance" }, { "id": 878, "name": "Science Fiction" }, { "id": 10770, "name": "TV Movie" }, { "id": 53, "name": "Thriller" }, { "id": 10752, "name": "War" }, { "id": 37, "name": "Western" }]
+var movieArray = [];
+var searchingMovieArray = [];
+var keywordArray = [];
+var keywordIdArray = [];
+var genreId = 0;
+var genre;
+var movieObjectArray = [];
 // referense to DB
 
+
+//Converts emotion spectrum into a genre
+
+function emoteGenre(input) {
+    if (input.Angry > 0.3) {
+        return "war"
+    }
+    if (input.Happy > 0.3) {
+        return "animation";
+    }
+    if (input.Fear > 0.3) {
+        return "thriller";
+    }
+    if (input.Sad > 0.3) {
+        return "comedy"
+    }
+    if (input.Surprise > 0.3) {
+        return "mystery"
+    }
+    if (input.Angry > 0.2 && input.Surprise > .2) {
+        return "crime"
+    }
+    if (input.Happy > .2 && input.Surprise > .2) {
+        return "action"
+    }
+    if (input.Fear > .2 && input.Surprise > .2) {
+        return "horror"
+    }
+    if (input.Sad > .2 && input.Surprise > .2) {
+        return "fantasy"
+    }
+    if (input.Angry > .2 && input.Happy > .2) {
+        return "adventure"
+    }
+    if (input.Angry > .2 && input.Fear > .2) {
+        return "western"
+    }
+    if (input.Angry > .2 && input.Happy > .2) {
+        return "adventure"
+    }
+    if (input.Happy > .2 && input.Fear > .2) {
+        return "romance"
+    }
+    if (input.Sad > .2 && input.Fear > .2) {
+        return "drama"
+    }
+    if (input.Angry > .2 && input.Sad > .2) {
+        return "music"
+    } else {
+        return "tv movie"
+    }
+}
 
 // Get image fron user 
 
@@ -64,7 +126,7 @@ function addPicToPage(img) {
     }
     imgId++;
     //
-    $('.del_img').on("click", function() {
+    $('.del_img_but').on("click", function() {
         var id = $(this).data('id');
         $(this).closest('.images').remove();
         threeImg = threeImg.filter(function(element) {
@@ -90,8 +152,8 @@ function threeImgIndico() {
         ).then(function(response) {
             var resp = JSON.parse(response);
             var stuff = getHighestKey(resp.results);
-            // console.log("stuff", stuff);
             imagesKeywords.push(stuff.split(', ')[0]);
+            keywordArray.push(stuff.split(', ')[0]);
             trigger--;
         });
     });
@@ -107,17 +169,16 @@ function avatarIndico() {
         })
     ).then(function(response) {
         var resp = JSON.parse(response);
-        // console.log("avatar obj: ", resp)
         var emotion = resp.results["0"].emotions;
-        facialKeyword = getHighestKey(emotion);
         facialKeywordObject = emotion;
+        genre = emoteGenre(emotion)
         trigger--;
     });
 }
 
 function paragraphIndico() {
     $.post(
-        'https://apiv2.indico.io/emotion',
+        'https://apiv2.indico.io/keywords?version=2',
         JSON.stringify({
             'api_key': indicoAPIkey,
             'data': paragraph,
@@ -125,10 +186,29 @@ function paragraphIndico() {
         })
     ).then(function(response) {
         var resp = JSON.parse(response);
-        paragraphKeyword = getHighestKey(resp.results);
         paragraphKeywordObject = resp.results;
+        sortedParagraphKeywordArray = sortProperties(paragraphKeywordObject);
+        keywordArray.push(sortedParagraphKeywordArray[0][0]);
+        keywordArray.push(sortedParagraphKeywordArray[1][0]);
+        keywordArray.push(sortedParagraphKeywordArray[2][0]);
+        keywordArray.push(sortedParagraphKeywordArray[3][0]);
+        keywordArray.push(sortedParagraphKeywordArray[4][0]);
         trigger--;
     });
+}
+
+function sortProperties(obj) {
+    // convert object into array
+    var sortable = [];
+    for (var key in obj)
+        if (obj.hasOwnProperty(key))
+            sortable.push([key, obj[key]]); // each item is an array in format [key, value]
+
+        // sort items by value
+    sortable.sort(function(a, b) {
+        return b[1] - a[1]; // compare numbers
+    });
+    return sortable; // array in format [ [ key1, val1 ], [ key2, val2 ], ... ]
 }
 
 function getHighestKey(response) {
@@ -141,6 +221,115 @@ function getHighestKey(response) {
         }
     }
     return highestKey;
+}
+
+/* --------------------------The MovieDB Functions ------------*/
+
+function genreFinder() {
+    genreArray.forEach(function(nameG) {
+        if (genre === nameG.name.toLowerCase()) {
+            genreId = nameG.id;
+        }
+    })
+};
+
+function idKeyword() {
+    var keywordURL;
+    var settings;
+    genreFinder();
+    for (var i = 0; i < keywordArray.length; i++) {
+        keywordURL = "https://api.themoviedb.org/3/search/keyword?api_key=4eea97057056c04b2e278ac4baa59a43&query=" + keywordArray[i] + "&page=1"
+        settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": keywordURL,
+            "method": "GET",
+            "headers": {},
+            "data": "{}",
+        }
+
+        $.ajax(settings).done(function(response) {
+            //pull first keyword id number from array
+            if (response.results[0] !== undefined) {
+                keywordIdArray.push(response.results[0].id);
+                var thisKeywordId = response.results[0].id;
+                var movieIdURL;
+                var settings;
+
+                //search for movies with proper keyword and genre
+                movieIdURL = "https://api.themoviedb.org/3/discover/movie?api_key=4eea97057056c04b2e278ac4baa59a43&language=en-US&sort_by=popularity.desc&page=1&vote_average.gte=6.5&with_genres=" + genreId + "&with_keywords=" + thisKeywordId;
+
+                settings = {
+                    "url": movieIdURL,
+                };
+                //Pulls films from each query and push their JSON Data to a new array
+                $.ajax(settings).done(function(response) {
+                    console.log(response);
+                    if (response.total_results > 4) {
+                        movieGetter(response.results[0].id);
+                        movieGetter(response.results[1].id);
+                        movieGetter(response.results[2].id);
+                        movieGetter(response.results[3].id);
+                        movieGetter(response.results[4].id);
+                    } else if (response.total_results > 3) {
+                        movieGetter(response.results[0].id);
+                        movieGetter(response.results[1].id);
+                        movieGetter(response.results[2].id);
+                        movieGetter(response.results[3].id);
+                    } else if (response.total_results > 2) {
+                        movieGetter(response.results[0].id);
+                        movieGetter(response.results[1].id);
+                        movieGetter(response.results[2].id);
+                    } else if (response.total_results > 1) {
+                        movieGetter(response.results[0].id);
+                        movieGetter(response.results[1].id);
+                    } else if (response.total_results > 0) {
+                        movieGetter(response.results[0].id);
+                    } else {}
+                });
+            } else {
+                return;
+            }
+        });
+    };
+    movieShuffle();
+}
+
+
+
+var testArray = movieArray;
+
+function movieShuffle() {
+    for (var i = 0; i < movieArray.length; i++) {
+        var movieNumber;
+
+        //Push randomized movie into searching array
+        movieNumber = movieArray[Math.floor(Math.random() * movieArray.length)];
+        searchingMovieArray.push(movieArray[movieNumber]);
+        //Remove added film from original array so duplicates dont occur
+        movieArray.splice(movieNumber, 1);
+    }
+}
+
+//Pushes the JSON data of a movie to the movieObjectArray
+function movieGetter(thisID) {
+    var movieURL = "https://api.themoviedb.org/3/movie/" + thisID + "?api_key=4eea97057056c04b2e278ac4baa59a43&language=en-US"
+    var settings;
+
+    settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": movieURL,
+        "method": "GET",
+        "headers": {},
+        "data": "{}",
+    }
+
+    $.ajax(settings).done(function(response) {
+        movieObjectArray.push(response);
+        showSuggestions(response);
+    });
+
 }
 
 $('#getRecomendedMovies').on('click', function() {
@@ -164,7 +353,21 @@ $('#getRecomendedMovies').on('click', function() {
             console.log("****************************************************");
             console.log("paragraphKeywordObject: ");
             console.log(paragraphKeywordObject);
-            // ALL INDICO QUERIES DONE. CAN START QUERING MOVIES API
+            idKeyword();
+
         }
     }
-})
+});
+
+
+
+
+//https://api.themoviedb.org/3/movie/76341?api_key={4eea97057056c04b2e278ac4baa59a43}
+
+
+//Add genre to search query
+
+
+//Find keywords Id numbers
+
+//Search for each ID
